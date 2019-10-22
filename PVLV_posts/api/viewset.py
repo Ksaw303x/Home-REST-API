e749f3be846f16e98c7b8182776c1727fab1af20
simone <not.simone96@gmail.com>
2019-10-22 16:29:40 +0200

    Added CORS with allow any Added Authentication status

--- PVLV_posts/api/viewset.py
+++ PVLV_posts/api/viewset.py
@@ -1,14 +1,19 @@
 from PVLV_posts.models import Post
 from PVLV_posts.api.serializers import PostSerializer
 
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 
 class PostSnippetViewSet(ModelViewSet):
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (AllowAny,)
 
     @action(methods=['get'], detail=False)
     def newest(self, request):
